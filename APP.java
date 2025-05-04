f167b23870ae24f582fb3a3ff5ac5e1b08fa6d76
Adrian Hustea <adriannicolash3@gmail.com>
2025-05-04 02:11:47 +0200

    Create APP.java

---
+++ APP.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class APP {
+
+    /**
+     * Default constructor
+     */
+    public APP() {
+    }
+
+
+    /**
+     * 
+     */
+    public void main() {
+        // TODO implement here
+    }
+
+}
